5a50072fa96544dcf37feac4415208fb057c428a
SeaScript1 <95991765+SeaScript1@users.noreply.github.com>
2023-05-12 12:44:18 -0700

    Delete buttons added to UI, non-functional

--- src/main/java/sayItAssistant/components/Footer.java
+++ src/main/java/sayItAssistant/components/Footer.java
@@ -31,6 +31,8 @@ import javax.swing.JPanel;
 ||          Field:
 ||					speakNewQuestion - new question button
 ||					stopRecording - stop recording button
+||                  deleteCurrent - delete current question button
+||                  deleteAll - delete all questions button
 ||					recordingStatus - true if recording is in progress. False otherwise
 ||					targetDataLine - type of dataline from which audio can be read
 ||					audioFormat - format for the audio
@@ -54,6 +56,8 @@ import javax.swing.JPanel;
 public class Footer extends JPanel { // This class contains recording buttons
     JButton speakNewQuestion;
     JButton stopRecording;
+    JButton deleteCurrent;
+    JButton deleteAll;
     boolean recordingStatus = false;
     private TargetDataLine targetDataLine;
     private AudioFormat audioFormat;
@@ -214,6 +218,8 @@ public class Footer extends JPanel { // This class contains recording buttons
     |  Constructor Footer()
     |
     |         Purpose: Creates Footer with the new question and stop recording buttons
+    |                  on the left side and delete current question and delete all on 
+    |                  on the right side
     |
     |   Pre-condition: None
     |
@@ -224,20 +230,22 @@ public class Footer extends JPanel { // This class contains recording buttons
     |         Returns: None
     *-------------------------------------------------------------------*/
     public Footer() {
+
+        this.setPreferredSize(new Dimension(270, 35));
+
         // Set Question recording buttons to the left of the footer
         JPanel leftHalf = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
-        this.setPreferredSize(new Dimension(270, 35));
         // Create new question button
         speakNewQuestion = new JButton("New Question");
         // Set Font
-        speakNewQuestion.setFont(new Font("Segoe Script", Font.BOLD, 10));
+        speakNewQuestion.setFont(new Font("Sans-serif", Font.BOLD, 10));
         speakNewQuestion.setBackground(Color.WHITE);
         leftHalf.add(speakNewQuestion);
 
         // Create stop recording button
         stopRecording = new JButton("Stop Recording");
         // Set Font
-        stopRecording.setFont(new Font("Segoe Script", Font.BOLD, 10));
+        stopRecording.setFont(new Font("Sans-serif", Font.BOLD, 10));
         stopRecording.setBackground(Color.WHITE);
         leftHalf.add(stopRecording);
 
@@ -246,7 +254,25 @@ public class Footer extends JPanel { // This class contains recording buttons
         setLayout(new BorderLayout());
         this.add(leftHalf, BorderLayout.WEST);
 
-        //TODO: Add a right half for delete buttons. Follows a similar format as above
+        // Set Delete buttons to the right of the footer
+        JPanel rightHalf = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 0));
+        // Create delete current question button
+        deleteCurrent = new JButton("Delete");
+        // Set features
+        deleteCurrent.setFont(new Font("Sans-serif", Font.BOLD, 10));
+        deleteCurrent.setBackground(Color.WHITE);
+        rightHalf.add(deleteCurrent);
+
+        // Create clear all questions button
+        deleteAll = new JButton("Clear All");
+        // Set features
+        deleteAll.setFont(new Font("Sans-serif", Font.BOLD, 10));
+        deleteAll.setBackground(Color.RED);
+        rightHalf.add(deleteAll);
+
+        //Adjust position
+        rightHalf.setBorder(BorderFactory.createEmptyBorder(5,0,5,0));
+        this.add(rightHalf, BorderLayout.EAST);
 
         audioFormat = getAudioFormat();
         
