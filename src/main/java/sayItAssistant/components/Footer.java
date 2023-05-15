a7f46f515b243abeca5737c45588edaabaff323c
Varun Durai <vardurun@gmail.com>
2023-05-15 06:55:45 -0700

    Fixed deletion bug

--- src/main/java/sayItAssistant/components/Footer.java
+++ src/main/java/sayItAssistant/components/Footer.java
@@ -109,15 +109,17 @@ public class Footer extends JPanel { // This class contains recording buttons
 
         deleteCurrent.addActionListener(
             (ActionEvent e)-> {
-            	try {
+                try {
+                    if(!Sidebar.historyJList.isSelectionEmpty())  {
                     String query = String.valueOf(Sidebar.getIndex());
                     URL url = new URL(URL + "?=" + query);
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.setRequestMethod("DELETE");
                     conn.getInputStream();
-                  } catch (Exception ex) {
+                    }
+                } catch (Exception ex) {
                     ex.printStackTrace();
-                  }
+                }
             Sidebar.updateRemoveHistory();
             }
         );
