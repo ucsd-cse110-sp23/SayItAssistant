70cef77377707a17d70ccc8d6c80045b5de907a2
cassponmal <103294645+cassponmal@users.noreply.github.com>
2023-06-05 17:54:29 -0700

    JFrame Setup

---
+++ src/main/java/sayItAssistant/components/Email.java
@@ -0,0 +1,16 @@
+package sayItAssistant.components;
+package sayItAssistant.components;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class Email  extends JFrame {
+   
+    private FieldPanel fieldPanel;
+    public 
+
+    
+}
