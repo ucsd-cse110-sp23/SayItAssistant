a012b77fd1281c8e0d5cca6c7e151277a9138e17
cassponmal <103294645+cassponmal@users.noreply.github.com>
2023-06-05 18:09:27 -0700

    Update Email.java

--- src/main/java/sayItAssistant/components/Email.java
+++ src/main/java/sayItAssistant/components/Email.java
@@ -1,11 +1,73 @@
 package sayItAssistant.components;
-package sayItAssistant.components;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import javax.swing.*;
+
+
+
+class FieldPanel extends JPanel {
+    
+    private JTextField firstNameField, lastNameField, displayNameField,emailAddressField, SMTPField, TLSPortField, emailPasswordField;
+    
+    public FieldPanel() {
+        setLayout(new GridLayout(3, 2));
+        JLabel firstNameLabel = new JLabel("First Name:");
+        firstNameField = new JTextField();
+        add(firstNameLabel );
+        add(firstNameField);
+
+        JLabel lastNameLabel = new JLabel("Last Name:");
+        lastNameField = new JTextField();
+        add(lastNameLabel);
+        add(lastNameField);
+
+        JLabel displayNameLabel = new JLabel("Display Name:");
+        displayNameField = new JTextField();
+        add(displayNameLabel);
+        add(displayNameField);
+
+
+        JLabel emailAddressLabel = new JLabel("Email Address:");
+        emailAddressField = new JTextField();
+        add(emailAddressLabel);
+        add(emailAddressField);
+
+        JLabel SMTPLabel = new JLabel("SMTP:");
+        SMTPField = new JTextField();
+        add(SMTPLabel);
+        add(SMTPField);
+
+        ////emailPasswordField;
+        JLabel TLSPortLabel = new JLabel("TLS Port:");
+        TLSPortField = new JTextField();
+        add(emailAddressLabel);
+        add(emailAddressField);
+
+        JLabel emailpasswordLabel = new JLabel("Password:");
+        emailPasswordField = new JTextField();
+        add(emailpasswordLabel);
+        add(emailPasswordField);
+
+        JLabel verifyLabel = new JLabel("Verify Password:");
+        verifyField = new JTextField();
+        add(verifyLabel);
+        add(verifyField);
+    }
+    public String getEmail() {
+        return emailField.getText();
+    }
+
+    public String getPassword() {
+        return passwordField.getText();
+    }
+
+    public String getVerify() {
+        return verifyField.getText();
+    }
+}
 
 public class Email  extends JFrame {
    
