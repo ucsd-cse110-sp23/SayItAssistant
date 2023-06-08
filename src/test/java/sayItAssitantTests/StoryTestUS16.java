dee2f8014b7ee4bc92e889b9c288046b7aed922f
Bella Jeong <lahrry0925@gmail.com>
2023-06-08 04:50:49 -0700

    StoryTest US16

--- src/test/java/sayItAssitantTests/StoryTestUS16.java
+++ src/test/java/sayItAssitantTests/StoryTestUS16.java
@@ -1,34 +1,15 @@
-/*Creating Email [Iteration 2][Mid] [8]
-As a user, I want to use the Create Email voice command so that I can create an email
-
-Scenario1: Create an email prompt after setting up email
-Given: SayItAssistant2 is opened showing the main screen 
-		When: Helen clicks “Start” button and says “Create an email to Jill let’s meet at Geisel 
-for our 7 pm study session”
-			session” is shown and at the bottom of the email “Best Regards, HelloHellen” is
-			displayed
-
-Scenario2: Create an email prompt before setting up email
-Given: SayItAssistant2 is opened showing the main screen 
-		When: Helen clicks “Start” button and says “Create an email to Jill let’s meet at Geisel 
-for our 7 pm study session”
-		Then: In the main screen, “Create an email to Jill let’s meet at Geisel for out 7 pm study
-			session” is shown and at the bottom of the email “Best Regards, ______ ” with 
-			blank display name
- */
 package sayItAssitantTests;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
+import sayItAssistant.components.QAScreen;
 import sayItAssistant.data.Answer;
 import sayItAssistant.data.DataBase;
 import sayItAssistant.data.Question;
@@ -49,19 +30,88 @@ public class StoryTestUS16 {
 	||
 	||	Scenario 1: Create an email prompt after setting up email
 	||	Given: SayItAssistant2 is opened showing the main screen
+	||	When: Helen clicks “Start” button and says “Create email to Jill letlr’s meet at Geisel
+	||	for our 7 pm study session”
+	||	Then: In the main screen, “Create email to Jill let’s meet at Geisel for out 7 pm study
+	||		session” is shown and at the bottom of the email “HelloHellen” is
+	||		displayed
+	||
+	|+-----------------------------------------------------------------------
+	||
+	||	First initialize DataBase object and call signUp method same as our UI
+	||	This sets up the user's email account
+	||	Then create the email content and sender name
+	||	Then add the question to the database
+	||  Then create the QAScreen object
+	||  Verify that the expected email content is equal to the output string with sender name
+	||
+	++-----------------------------------------------------------------------*/
+
+	@Test
+	void scenarioOne(){
+		DataBase db = new MockDataBase();
+		boolean signUpResult = db.signUp("helenk@ucsd.edu","hk12345");
+		assertEquals(true, signUpResult);
+
+		String email = "Create email to Jill let’s meet at Geisel for our 7 pm study session";
+		String sender = "HelloHellen";
+		
+		Question question = new Question();
+		question.setQuestionString(email);
+		Answer answer = new Answer();
+		answer.setAnswerString(sender);
+		question.setAnswerObject(answer);
+
+		db.addQuestion(question);
+		QAScreen qaScreen = new QAScreen();
+
+		String outputString = QAScreen.QAText.getText();
+		assertEquals("Create email to Jill let’s meet at Geisel for our 7 pm study session", outputString);
+	}
+
+	/*+----------------------------------------------------------------------
+	||
+	||	Scenario 2: Create an email prompt before setting up email
+	||	Given: SayItAssistant2 is opened showing the main screen
 	||	When: Helen clicks “Start” button and says “Create an email to Jill let’s meet at Geisel
 	||	for our 7 pm study session”
 	||	Then: In the main screen, “Create an email to Jill let’s meet at Geisel for out 7 pm study
-	||		session” is shown and at the bottom of the email “Best Regards, HelloHellen” is
-	||		displayed
+	||		session” is shown and at the bottom of the email “ ______ ” with
+	||		blank display name
 	||
-	|+-----------------------------------------------------------------------*/
+	|+-----------------------------------------------------------------------
+	||
+	||	First initialize DataBase object and call signUp method same as our UI
+	||	This sets up the user's email account
+	||	Then create the email content without sender name
+	||	Then add the question to the database
+	||  Then create the QAScreen object
+	||  Verify that the expected email content is equal to the output string with blank sender name
+	||
+	++-----------------------------------------------------------------------*/
 
 	@Test
-	void scenarioOneTest() {
+	void scenarioTwo(){
 		DataBase db = new MockDataBase();
-		boolean signUpResult = db.signUp("helenk@ucsd.edu", "hk12345");
+		boolean signUpResult = db.signUp("helenk@ucsd.edu","hk12345");
 		assertEquals(true, signUpResult);
-		}	
-  
+
+		String email = "Create email to Jill let’s meet at Geisel for our 7 pm study session";
+		String sender = " ______ ";
+
+		Question question = new Question();
+		question.setQuestionString(email);
+		Answer answer = new Answer();
+		answer.setAnswerString(sender);
+		question.setAnswerObject(answer);
+
+		db.addQuestion(question);
+		QAScreen qaScreen = new QAScreen();
+
+		String outputString = QAScreen.QAText.getText();
+		assertEquals("Create email to Jill let’s meet at Geisel for our 7 pm study session", outputString);
+	}
+
+
+
 }
